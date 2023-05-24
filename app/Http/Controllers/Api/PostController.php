96cf2a1510f2fd8bd06388c4cc23602265db1cd2
Mohammed AD <moad90300@gmail.com>
2023-05-24 14:09:03 +0300

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    use ApiResponseTrait;
+
+    public function index(){
+//        $posts = Post::get();
+        $posts = PostResource::collection(Post::get()); // block data
+        return $this->apiResponse($posts,'ok',200);
+    }
+
+    public function show($id){
+
+        $post = Post::find($id); // one element in data
+
+        if($post){
+            return $this->apiResponse(new PostResource($post),'ok',200);
+        }
+
+        return $this->apiResponse(null,'The Post Not Found',401);
+
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null,$validator->errors(),400);
+        }
+
+        $post = Post::create($request->all());
+
+        if($post){
+            return $this->apiResponse(new PostResource($post),'The post Save',201);
+        }
+
+        return $this->apiResponse(null,'The post Not Save',400);
+    }
+
+
+    public function update(Request $request , $id){
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null,$validator->errors(),400);
+        }
+
+        $post = Post::find($id);
+
+        if(!$post){
+            return $this->apiResponse(null,'The post Not Found',404);
+        }
+
+        $post->update($request->all());
+
+        if($post){
+            return $this->apiResponse(new PostResource($post),'The post Update',201);
+        }
+
+
+    }
+    public function destroy($id){
+        $post = Post::find($id);
+
+        if(!$post){
+            return $this->apiResponse(null,'The post Not Found',404);
+        }
+
+        $post->destroy($id);
+
+        if($post){
+            return $this->apiResponse(new PostResource($post),'The post Deleted',200);
+        }
+    }
+}
